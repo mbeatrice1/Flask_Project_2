915defb015e375dd5b641b434ec716a353fe9a79
mbeatrice1 <mahorobeatrice9@gmail.com>
2021-03-24 21:08:49 +0200

    added title

---
+++ app/views.py
@@ -0,0 +1,14 @@
+from flask import render_template
+from app import app
+
+
+# Views
+@app.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    title = 'WELCOME TO NEWS ARTICLES'
+    return render_template('index.html',title = title)
