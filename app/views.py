5f874b38f9bbbfd7f1eea0b96c795a9d5d2514ce
mbeatrice1 <mahorobeatrice9@gmail.com>
2021-03-24 21:18:12 +0200

    added news page

--- app/views.py
+++ app/views.py
@@ -10,5 +10,13 @@ def index():
     View root page function that returns the index page and its data
     '''
 
-    title = 'WELCOME TO NEWS ARTICLES'
+    title = 'newsArticles'
     return render_template('index.html',title = title)
+
+@app.route('/news/<int:news_id>')
+def news(news_id):
+
+    '''
+    View news page function that returns the news details page and its data
+    '''
+    return render_template('news.html',id = news_id)
