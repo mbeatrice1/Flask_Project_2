4c649d80291bc7948968957a55918e2388e0a371
mbeatrice1 <mahorobeatrice9@gmail.com>
2021-03-25 16:21:00 +0200

    added source test

--- app/views.py
+++ app/views.py
@@ -1,22 +1,70 @@
 from flask import render_template
 from app import app
-
+from .request import get_sources
+from .request import get_news_from_source
 
 # Views
-@app.route('/')
-def index():
+#@app.route('/')
+#def index():
 
     '''
-    View root page function that returns the index page and its data
+ View root page function that returns the index page and its data
     '''
 
-    title = 'newsArticles'
-    return render_template('index.html',title = title)
+    #title = 'newsArticles'
+    #return render_template('index.html',title = title)
 
-@app.route('/news/<int:news_id>')
-def news(news_id):
+#@app.route('/news/<int:news_id>')
+#def news(news_id):
 
     '''
     View news page function that returns the news details page and its data
     '''
-    return render_template('news.html',id = news_id)
+    #return render_template('news.html',id = news_id)
+
+
+
+
+@app.route("/")
+def index():
+    '''
+    View root function that returns index and page and its data
+    '''
+
+    # Getting sport sources
+    sport_sources = get_sources('sports')
+
+    # Getting business sources
+    business_sources = get_sources('business')
+
+    # Getting entertainment sources
+    # entertainment_sources = get_sources('entertainment')
+
+    # Getting general sources
+    general_sources = get_sources('general')
+
+    # Getting health sources
+    health_sources = get_sources('health')
+
+    # Getting science sources
+    science_sources = get_sources('science')
+
+    # Getting technology sources
+    technology_sources = get_sources('technology')
+
+
+    title = "Home - News Article"
+
+    return render_template("index.html", title = title, sports = sport_sources, general = general_sources, technology = technology_sources, health = health_sources, science = science_sources, business = business_sources)
+
+@app.route("/article/<source_id>")
+def source(source_id):
+    '''
+    View route function that returns source page showing all articles of a given source
+    '''
+
+    title = f'{source_id}'
+    articles_list = get_articles_from_source(source_id)
+
+    return render_template('source.html',title = title, articles = articles_list)
+    
